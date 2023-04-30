b68a02d3ff1f276bd87a6b39df364a46c543c4a7
RafaelBP02 <rafaelbertopereira@gmail.com>
2023-04-30 08:34:06 -0300

    incluindo a assinatura de modulo em Menu.hs

--- src/Menu.hs
+++ src/Menu.hs
@@ -1,2 +1,8 @@
+module Menu (startGameMenu, inGameMenu) where
+
+import Data.Array.IO
+import Control.Monad
+
 startGameMenu :: Int -> IO ()
+
 inGameMenu :: Int -> Int -> [([Char], Char)] -> [([Char], Char)] -> [([Char], Char)] -> IO ()
