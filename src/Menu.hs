0cde59bfa1f01e2cedf3d0724265fffa4a77c259
RafaelBP02 <rafaelbertopereira@gmail.com>
2023-05-03 09:57:52 -0300

    Correcao de bugs e finalizacao a funcao startGameMenu do modulo Menu

--- src/Menu.hs
+++ src/Menu.hs
@@ -1,27 +1,14 @@
 -- module Menu (startGameMenu, inGameMenu, mainMenu) where
-module Menu (mainMenu, verifyMoney) where
+module Menu (mainMenu, isMoneyEnough, startGameMenu, inGameMenu) where
 
+import Cards
 import Control.Monad
 import Data.Array.IO
 
--- mainMenu :: Int -> IO ()
--- mainMenu money = do
---   if money <= 0
---     then putStrLn "Você faliu...\n"
---     else do
---       putStrLn "Menu Principal do Jogo: "
---       putStrLn "1 - Iniciar partida"
---       putStrLn "2 - Fechar\n"
---       option <- getLine
---       case option of
---         "1" -> startGameMenu money
---         "2" -> putStrLn "O jogo serah finalizado...\n"
-
 isMoneyEnough :: Int -> Int -> Bool
-
-verifyMoney money minValue
-  | money <= minValue = false
-  | otherwise = true
+isMoneyEnough money minValue
+  | money <= minValue = False
+  | otherwise = True
 
 mainMenu :: Int -> IO ()
 mainMenu money = do
@@ -42,7 +29,7 @@ startGameMenu money = do
   putStrLn "3 - Apostar 100"
   putStrLn "4 - Apostar 250"
   putStrLn "5 - Apostar 500"
-  putStrLn "6 - Voltar para o menu principal\n"
+  putStrLn "6 - Voltar para o menu principal"
 
   option <- getLine
   deckShuffled <- shuffle deck
@@ -52,8 +39,50 @@ startGameMenu money = do
   case option of
     "1" -> do
       if isMoneyEnough money 10
-        then putStrLn $ "Apostando: " ++ show 10 inGameMenu 10 (money -10) playerHand dealerHand _deckShuffled
-        else putStrLn "Dinheiro insuficiente para a aposta minima. Você Faliu"
+        then do 
+          putStrLn $ "Apostando: " ++ show 10
+          inGameMenu 10 (money -10) playerHand dealerHand _deckShuffled
+        else do
+          putStrLn "Dinheiro insuficiente para a aposta minima. Você QUEBROU!"
+          putStrLn "\n\n\nIniciando nova partida... \n\n\n\nSelecione o que deseja fazer:\n"
+          mainMenu 2000
+    "2" -> do
+      if isMoneyEnough money 50
+        then do
+          putStrLn $ "Apostando: " ++ show 50
+          inGameMenu 50 (money -50) playerHand dealerHand _deckShuffled
+        else do
+          putStrLn "\n\n\n\n\nDinheiro insuficiente. Escolha uma aposta de menor valor:"
+          startGameMenu money
+    "3" -> do
+      if isMoneyEnough money 100
+        then do
+          putStrLn $ "Apostando: " ++ show 100
+          inGameMenu 100 (money -100) playerHand dealerHand _deckShuffled
+        else do
+          putStrLn "\n\n\n\n\nDinheiro insuficiente. Escolha uma aposta de menor valor:"
+          startGameMenu money
+    "4" -> do
+      if isMoneyEnough money 250
+        then do
+          putStrLn $ "Apostando: " ++ show 250
+          inGameMenu 250 (money -250) playerHand dealerHand _deckShuffled
+        else do
+          putStrLn "\n\n\n\n\nDinheiro insuficiente. Escolha uma aposta de menor valor:"
+          startGameMenu money
+    "5" -> do
+      if isMoneyEnough money 500
+        then do
+          putStrLn $ "Apostando: " ++ show 500
+          inGameMenu 500 (money -500) playerHand dealerHand _deckShuffled
+        else do
+          putStrLn "\n\n\n\n\nDinheiro insuficiente. Escolha uma aposta de menor valor:"
+          startGameMenu money
+          
+    "6" -> do
+      putStrLn ("escolheu 6")
+      mainMenu 2000
+
 
 -- exemplo de passagem de parametros da funcao abaixo:
 -- inGameMenu 5 2000 [("Hearts", '2'), ("Diamonds", '7')] [("Hearts", '4'), ("Diamonds", '5')] [("Hearts", 'X'), ("Diamonds", 'X')]
